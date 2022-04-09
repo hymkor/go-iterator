a09adcb61cb60255e5a37b274f2a6fb970b1b3e9
HAYAMA_Kaoru <iyahaya@nifty.com>
2022-04-09 18:28:29 +0900

    example2(SortedRange) prototype

--- example2.go
+++ example2.go
@@ -4,11 +4,14 @@
 package main
 
 import (
+	"io"
 	"sort"
+
+	"github.com/hymkor/go-iterator"
 )
 
 type HasOrder interface {
-	~string | ~int | ~int8 | ~int16 | ~int32 | ~int64
+	~string | ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~float32 | ~float64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
 type KeyValuePair[K HasOrder, V any] struct {
@@ -16,7 +19,7 @@ type KeyValuePair[K HasOrder, V any] struct {
 	Value V
 }
 
-func SortedRange[K HasOrder, V any](m map[K]V) []KeyValuePair[K, V] {
+func SortedRange[K HasOrder, V any](m map[K]V) *iterator.Iterator[KeyValuePair[K, V]] {
 	pairs := make([]KeyValuePair[K, V], 0, len(m))
 	for key, val := range m {
 		pairs = append(pairs, KeyValuePair[K, V]{Key: key, Value: val})
@@ -26,19 +29,32 @@ func SortedRange[K HasOrder, V any](m map[K]V) []KeyValuePair[K, V] {
 		return pairs[i].Key < pairs[j].Key
 	})
 
-	return pairs
+	return iterator.New(func() (KeyValuePair[K, V], error) {
+		if len(pairs) <= 0 {
+			return KeyValuePair[K, V]{}, io.EOF
+		}
+		value := pairs[0]
+		pairs = pairs[1:]
+		return value, nil
+	})
 }
 
 func main() {
 	sample := map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
-		"four":  4,
+		"A": 1,
+		"B": 2,
+		"C": 3,
+		"D": 4,
 	}
 
-	kv := SortedRange(sample)
-	for _, p := range kv {
-		println(p.Key, p.Value)
+	println("--- for-range ---")
+	for key, val := range sample {
+		println(key, val)
+	}
+
+	println("--- SortedRange ---")
+	p := SortedRange(sample)
+	for p.Next() {
+		println(p.Value.Key, p.Value.Value)
 	}
 }
